c76ac030da6547ca8d98928c3ff0f36325003d02
Service Web <sw@gssmail.com>
2024-03-11 14:47:15 -0500

    Commit changes in C:\Websites\Git\emarlow\4324\GCG_4324_SO_Change.sql

---
+++ GCG_4324_SO_Change.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_4324_SO_Change"(
+ "Order_No" CHAR(7),
+ "Order_Line" CHAR(4),
+ "Quote_No" CHAR(7),
+ "Quote_Line" CHAR(4),
+ "UM" CHAR(2) NOT NULL,
+ "Weight" DOUBLE,
+ "Price" DOUBLE,
+ "Selected_UM" BIT NOT NULL);
